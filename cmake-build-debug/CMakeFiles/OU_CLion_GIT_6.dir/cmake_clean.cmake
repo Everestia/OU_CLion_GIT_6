276ec4c71594d513ce620dee1f87846ee741a069
Everestia <agnieszka.satarowska@dokt.p.lodz.pl>
2022-01-18 16:27:10 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OU_CLion_GIT_6.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU_CLion_GIT_6.dir/main.cpp.obj"
+  "OU_CLion_GIT_6.exe"
+  "OU_CLion_GIT_6.exe.manifest"
+  "OU_CLion_GIT_6.pdb"
+  "libOU_CLion_GIT_6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OU_CLion_GIT_6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/OU_CLion_GIT_6.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for OU_CLion_GIT_6.
+# This may be replaced when dependencies are built.
